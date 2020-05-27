632197ae1500c6db497efe6a7e075c462bb4ef02
njm222 <nathan.john.menezes@gmail.com>
2020-05-27 10:18:07 +0530

    added subtitle to portfolio

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -10,17 +10,17 @@
             <v-toolbar-title>
               Portfolio
             </v-toolbar-title>
-            <v-spacer/>
+            <v-spacer />
             <v-toolbar-items>
               <v-select
-                item-text="name"
                 v-model="defaultCategory"
+                item-text="name"
                 :label="defaultCategory.name"
                 :items="videoCategories"
                 class="d-flex align-center"
                 outlined
                 transition="scroll-y-transition"
-              ></v-select>
+              />
             </v-toolbar-items>
           </v-layout>
         </v-container>
@@ -35,20 +35,40 @@
               Here's a showcase of my work filler text and some more text for two lines of my work filler text.
             </div>
           </v-layout>
-          <v-divider/>
-          <div v-for="video in videosByCategory" :key="video.link">
-            <v-row class="my-8">
-              <v-layout>
-                <youtube :video-id="video.link" player-width="100%" player-height="100%" class="youtube-container col-5"></youtube>
-                <v-layout column class="col-6 offset-1 text-left" justify-center>
-                  <v-card-title>{{video.title}}</v-card-title>
-                  <v-card-text>{{video.description}}</v-card-text>
-                </v-layout>
-              </v-layout>
-            </v-row>
-            <v-row>
-              <v-divider/>
-            </v-row>
+          <v-divider />
+          <div ref="portfolioContainer">
+            <div v-for="(video, index) in videosByCategory" :key="video.link">
+              <div class="my-8">
+                <div ref="portfolioItems" class="item-container">
+                  <v-layout
+                    v-if="loading[index] === true"
+                    justify-center
+                    align-center
+                  >
+                    <v-progress-circular
+                      indeterminate
+                      color="primary"
+                    />
+                  </v-layout>
+                  <youtube
+                    :class="{ visible: !loading[index] }"
+                    :video-id="video.link"
+                    player-width="100%"
+                    player-height="100%"
+                    class="youtube-container"
+                    @ready="ready(index)"
+                  />
+                  <v-spacer />
+                  <v-layout column class="text-left" justify-center>
+                    <v-card-title>{{ video.title }}<br>{{ video.subtitle }}</v-card-title>
+                    <v-card-text>{{ video.description }}</v-card-text>
+                  </v-layout>
+                </div>
+              </div>
+              <div>
+                <v-divider />
+              </div>
+            </div>
           </div>
         </v-container>
       </div>
@@ -61,14 +81,27 @@ import videosQuery from '~/apollo/queries/video/videos'
 import videoCategoriesQuery from '~/apollo/queries/video/categories'
 
 export default {
-  data() {
+  data () {
     return {
       // Initialize an empty videos variable
       model: 'tab-portfolio-videos',
       videos: [],
       videoCategories: [],
-      defaultCategory: 'all'
+      defaultCategory: 'all',
+      loading: []
     }
+  },
+  computed: {
+    videosByCategory () {
+      return this.videos.filter((video) => {
+        return video.video_categories.some((category) => {
+          return category.name === this.defaultCategory
+        })
+      })
+    }
+  },
+  mounted () {
+    this.updatePortfolioItemsCount()
   },
   apollo: {
     videos: {
@@ -80,28 +113,44 @@ export default {
       query: videoCategoriesQuery
     }
   },
-  computed: {
-    videosByCategory: function () {
-      return this.videos.filter((video) => {
-        return video.video_categories.some((category) => {
-          return category.name === this.defaultCategory
-        })
-      })
+  methods: {
+    ready (index) {
+      this.$nextTick(this.$set(this.loading, index, false))
+    },
+    updatePortfolioItemsCount () {
+      const numOfVideos = this.videos.length
+      for (let i = 0; i < numOfVideos; i++) {
+        this.loading[i] = true
+      }
     }
-  },
+  }
 }
 </script>
 
 <style>
-  .youtube-container {
+  .item-container {
     width: 100%;
+    max-width: 100vw;
+    display: flex;
+    flex-direction: row;
+  }
+  .item-container .layout {
+    width: 48%;
+    margin: 0 1%;
+  }
+  .youtube-container {
+    align-self: center;
+    width: 48%;
+    margin: 0 1%;
     height: 0;
-    margin: auto;
     z-index: 1;
     position: relative;
-    padding-bottom: 23.4375%;
-    display: block;
+    padding-bottom: 27%;
+    display: none;
     overflow: hidden;
+  }
+  .youtube-container.visible {
+    display: block;
   }
   .youtube-container iframe {
     display: block;
@@ -112,13 +161,28 @@ export default {
     z-index: 5;
     position: absolute;
   }
-  .v-text-field .v-input__control {
-    height: 3.5rem;
-  }
   .v-select {
     min-width: 20rem;
   }
   .v-select.v-text-field input {
     width: 0;
+  }
+  @media only screen and (max-width: 768px) {
+    .v-select {
+      min-width: 16rem;
+    }
+    .item-container {
+      flex-direction: column;
+      text-align: center;
+    }
+    .youtube-container {
+      width: 90%;
+      margin: 0 5%;
+      padding-bottom: 50.625%;
+    }
+    .item-container .layout {
+      width: 90%;
+      margin: 0 5%;
+    }
   }
 </style>
